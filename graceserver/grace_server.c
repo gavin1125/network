2efb63f5ddf0d25948c36986397c2020476674e0
chenlh <13692238295@163.com>
2021-07-03 15:11:31 +0800

    add grace server,client

---
+++ graceserver/grace_server.c
@@ -0,0 +1,78 @@
+#include "lib/common.h"
+
+static int count;
+
+static void sig_int(int signo)
+{
+    printf("\nreceived %d datagrams\n", count);
+    exit(0);
+}
+
+int main(int argc, char **argv)
+{
+    int listenfd;
+    listenfd = socket(AF_INET, SOCK_STREAM, 0);
+
+    struct sockaddr_in server_addr;
+    bzero(&server_addr, sizeof(server_addr));
+    server_addr.sin_family = AF_INET;
+    server_addr.sin_addr.s_addr = htonl(INADDR_ANY);
+    server_addr.sin_port = htons(SERV_PORT);
+
+    int rt1 = bind(listenfd, (struct sockaddr *)&server_addr, sizeof(server_addr));
+    if (rt1 < 0)
+    {
+        error(1, errno, "bind failed ");
+    }
+
+    int rt2 = listen(listenfd, LISTENENQ);
+    if (rt2 < 0)
+    {
+        error(1, errno, "listen failed ");
+    }
+
+    signal(SIGINT, sig_int);
+    signal(SIGPIPE, SIG_IGN);
+
+    int confd;
+    struct sockaddr_in client_addr;
+    socklen_t client_len = sizeof(client_addr);
+
+    if ((confd = accept(listenfd, (struct sockaddr *)&client_addr, &client_len)) < 0)
+    {
+        error(1, errno, "bind failed ");
+    }
+
+    char message[MAXLINE];
+    count = 0;
+
+    for (;;)
+    {
+        int n = read(confd, message, MAXLINE);
+        if (n < 0)
+        {
+            error(1, 0, "error read");
+        }
+        else if (n == 0)
+        {
+            error(1, 0, "client closed \n");
+        }
+
+        message[n] = 0;
+        printf("received %d bytes:%s\n", n, message);
+        count++;
+
+        char send_line[MAXLINE + 3];
+        sprintf(send_line, "Hi,%s", message);
+
+        sleep(5);
+
+        int write_nc = send(confd, send_line, strlen(send_line), 0);
+        printf("send bytes:%d \n", write_nc);
+
+        if (write_nc < 0)
+        {
+            error(1, errno, "error write");
+        }
+    }
+}
--- lib/common.h
+++ lib/common.h
@@ -1,15 +1,19 @@
-#include    <sys/types.h> 
-#include    <stdio.h>
-#include    <stdlib.h>
-#include    <sys/socket.h>
-#include    <netinet/in.h> 
-#include    <arpa/inet.h>  
-#include    <errno.h>
-#include    <unistd.h>
-#include    <strings.h> 
-#include    <string.h>
-
+#include <sys/types.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <sys/socket.h>
+#include <netinet/in.h>
+#include <arpa/inet.h>
+#include <errno.h>
+#include <unistd.h>
+#include <strings.h>
+#include <string.h>
+#include <signal.h>
 
 size_t readn(int fd, void *vptr, size_t n);
 
 void error(int status, int err, char *fmt, ...);
+
+#define SERV_PORT 43211
+#define MAXLINE 4096
+#define LISTENENQ 1024
